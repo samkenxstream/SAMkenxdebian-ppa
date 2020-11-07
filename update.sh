cbbd2af1a0f3c13b5ed6fe16fe8396618f50993c
friendlyanon <friendlyanon@users.noreply.github.com>
2020-11-07 16:05:37 +0000

    Use a distribution hierarchy

--- update.sh
+++ update.sh
@@ -4,10 +4,15 @@
 # https://assafmo.github.io/2019/05/02/ppa-repo-hosted-on-github.html
 
 # Packages & Packages.gz
-dpkg-scanpackages --multiversion . > simdjson/Packages
-gzip -k -f simdjson/Packages
+dpkg-scanpackages --multiversion . > dists/simdjson/binary-ppc64el/Packages
+gzip -k -f dists/simdjson/binary-ppc64el/Packages
 
 # Release, Release.gpg & InRelease
-apt-ftparchive release . > simdjson/Release
-gpg --default-key "$EMAIL" -abs -o - simdjson/Release > simdjson/Release.gpg
-gpg --default-key "$EMAIL" --clearsign -o - simdjson/Release > simdjson/InRelease
+cat - > dists/simdjson/Release <<EOF
+Suite: simdjson
+Codename: simdjson
+Architectures: ppc64el
+EOF
+apt-ftparchive release . >> dists/simdjson/Release
+gpg --default-key "$EMAIL" -abs -o - dists/simdjson/Release > dists/simdjson/Release.gpg
+gpg --default-key "$EMAIL" --clearsign -o - dists/simdjson/Release > dists/simdjson/InRelease
