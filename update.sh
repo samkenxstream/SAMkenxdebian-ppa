1f1c44ebfab544c84db3936cd1cbd1e1944a4680
friendlyanon <friendlyanon@users.noreply.github.com>
2020-11-08 04:00:03 +0000

    Regenerate using aptly

--- update.sh
+++ update.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env sh
 
-# Commands taken from:
-# https://assafmo.github.io/2019/05/02/ppa-repo-hosted-on-github.html
-
 # Packages & Packages.gz
 dpkg-scanpackages --multiversion . > dists/simdjson/main/binary-ppc64el/Packages
 gzip -k -f dists/simdjson/main/binary-ppc64el/Packages
 
 # Release, Release.gpg & InRelease
-cat - > dists/simdjson/Release <<EOF
+cd dists/simdjson
+
+rm Release Release.gpg InRelease
+
+apt-ftparchive release . > ../Release
+cat - ../Release > Release <<EOF
 Origin: simdjson
 Label: simdjson
 Suite: simdjson
 Codename: simdjson
-Description: simdjson
-Components: main
 Architectures: ppc64el
+Components: main
+Description: simdjson
 EOF
-apt-ftparchive release . >> dists/simdjson/Release
-gpg --default-key "$EMAIL" -abs -o - dists/simdjson/Release > dists/simdjson/Release.gpg
-gpg --default-key "$EMAIL" --clearsign -o - dists/simdjson/Release > dists/simdjson/InRelease
+
+rm ../Release
+
+gpg -abs -o - Release > Release.gpg
+gpg --clearsign -o - Release > InRelease
